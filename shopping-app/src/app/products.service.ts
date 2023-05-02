a840785b0dab440a417d63eab17617560d837d14
DA MAC 29 <damac29@DS-LEARNERs-MacBook-Air-29.local>
2023-05-02 10:09:20 +0200

    Added getProducts func in prod service

--- shopping-app/src/app/products.service.ts
+++ shopping-app/src/app/products.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-constructor() { }
+prodURL : string = 'https://fakestoreapi.com/products';  // URL to web api
+
+constructor(private http: HttpClient) { }
+
+getProducts() {
+  return this.http.get<any>(this.prodURL)
+}
 
 }
--- shopping-app/src/app/products/products.component.ts
+++ shopping-app/src/app/products/products.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products : any;
+
+  constructor(private productService: ProductsService) {} // inject a service here
+
+  ngOnInit() {
+    // this.productService.getProducts()
+    // .subscribe(res=>{this.Products=res;
+    //   console.log(this.Products);
+    // });
+    this.getProducts();
+   
+    
+  }
+
+  getProducts() {
+    this.productService.getProducts()
+    .subscribe(res=>{this.products=res;
+    console.log(this.products)
+  });
+    
+  }
 
 }
